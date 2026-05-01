dfa10d8f78cda29be7332d6f7b896cb6d846f7a9
AlevtynaShuhai <Sherbanalevtina@gmail.com>
2026-05-01 08:22:12 +0300

    Update events.js

--- resources/js/events.js
+++ resources/js/events.js
@@ -227,7 +227,7 @@ export const events = [
         id: 15,
         eventName: 'AT THE FOREST EDGE ACRYLIC CLASS',
         startDate: null,
-        date: '2026-06-24',
+        date: '2026-06-26',
         time: '6:00 pm - 9:00 pm',
         day: null,
         price: 84,
